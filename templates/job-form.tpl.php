e5620281908ed09ab5a321e906ec018832079da2
Shawna Staff <staffs@my.lanecc.edu>
2021-01-18 18:05:53 -0800

    added JavaScript function that toggles closing date in the form view when the open until filled option is selected or deselected, still working on how to hide/send empty string/or send ClosingDate__c as null. I believe this is going to be a PHP if statement or...possibly switch statement considering the form view functionality. I have tried a few ideas but I have not been successful at creating a job without a closing date nor editing the status of a posting's closing date to open until filled and vice-versa.

--- templates/job-form.tpl.php
+++ templates/job-form.tpl.php
@@ -1,10 +1,17 @@
 <?php
-	$classNames = $job["OpenUntilFilled__c"] ? "open-until-filled" : "";
-		
+$classNames = $job["OpenUntilFilled__c"] ? "open-until-filled" : "";
+
 ?>
 
-<form  class="<?php print $classNames; ?>"  method="post" action="/jobs/create">
-	
+<!--CSS to toggle closing date-->
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
+
+<form class="<?php print $classNames; ?>" name="form-jobs" method="post" action="/jobs/create">
+
 	<h2>Enter the job!</h2>
 
 	<label for="Name">Job Title</label><br />
@@ -24,18 +31,50 @@
 	<input type="date" name="PostingDate__c" id="PostingDate__c" value="<?php print $job["PostingDate__c"]; ?>" placeholder="Enter the date posted." />
 	<br /><br />
 
+	<!--this if statement allows the closingDate__c to be toggled repeatedly in form view-->
+	<?php if ($job["OpenUntilFilled__c"] == true) : ?>
+	<div  class="hidden" id="closingDate">
+	<?php else : ?>
+	<div id="closingDate">
+	<?php endif; ?>
 	<label for="ClosingDate__c">Closing Date</label><br />
 	<input type="date" name="ClosingDate__c" id="ClosingDate__c" value="<?php print $job["ClosingDate__c"]; ?>" placeholder="Enter the closing date." />
 	<br /><br />
+	</div>
 
-	<label for="OpenUntilFilled__c">Open Until Filled?</label>&nbsp&nbsp
-
-	<?php if($job["OpenUntilFilled__c"] == true): ?>
-		<input type="checkbox" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="<?php print $job["OpenUntilFilled__c"]; ?>" checked/>
-	<?php else: ?>
-		<input type="checkbox" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="<?php print $job["OpenUntilFilled__c"]; ?>"/>
-	<?php endif; ?>
-	<br /><br />
+	<div onclick="handleCheck()">
+		<label for="OpenUntilFilled__c">Open Until Filled?</label>&nbsp&nbsp
+		<?php if ($job["OpenUntilFilled__c"] == true) : ?>
+			<input type="checkbox" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="<?php print $job["OpenUntilFilled__c"]; ?>" checked />
+		<?php else : ?>
+			<input type="checkbox" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="<?php print $job["OpenUntilFilled__c"]; ?>" />
+		<?php endif; ?>
+		<br /><br />
+	</div>
 
 	<input type="submit" value="Save" />
 </form>
+
+<script>
+	//*Variables*//
+	let closingDate = document.getElementById("closingDate");
+
+	//*JavaScript function to toggle closing date in form view*//
+	function handleCheck() {
+
+	if (window.getComputedStyle(closingDate).display == "none") {
+		document.getElementById("closingDate").classList.remove('hidden');
+
+		//console.log("true"); //testTrash
+	}	
+    else {
+		document.getElementById("closingDate").classList.add('hidden');
+		document.getElementById("ClosingDate__c").innerHTML = null;
+		
+		//console.log("false");  //testTrash
+	}
+
+		
+			
+	}
+</script>
