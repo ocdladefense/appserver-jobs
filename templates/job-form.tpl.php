95b970582693b7739b8a30b80e2da06466fd5e08
Jose Bernal <jbernal.web.dev@gmail.com>
2021-03-19 09:27:55 -0700

    Add div wrappers to form elements.

--- templates/job-form.tpl.php
+++ templates/job-form.tpl.php
@@ -8,98 +8,114 @@ $classNames = $job["OpenUntilFilled__c"] ? "open-until-filled" : "";
 	.open-until-filled #closingDate {
 		display: none;
 	}
-	input[type="submit"],input[type="file"] {
+
+
+	label {
+		color: rgba(30,30,30,0.7);
+		font-size: 14px;
+	}
+	
+	.form-item {
+		margin-top: 27px;
+	}
+	
+	
+	button, input {
+		overflow: visible;
+		padding: 8px;
+		color: rgba(30,30,30,0.9);
+		width: 100%;
+	}	
+	
+	
+	input[type="submit"],
+	input[type="file"] {
 		display: block;
 		padding: 4px;
 	}
-	.table-header, .table-row{
-		margin-left: 50px;
-	}
-
 	
+	input[type=checkbox], input[type=radio] {
+			box-sizing: border-box;
+			padding: 0;
+			width: auto;
+	}
+ 
+
+	@media screen and (min-width:800px) {
+		.label {
+			display:none;
+		}
+		
+		.form-item {
+			max-width: 50%;
+		}
+	}
 </style>
 
 <form enctype="multipart/form-data" onsubmit="onSubmit();" class="<?php print $classNames; ?>" id="jobs-form" name="form-jobs" method="post" action="/jobs/create">
 
-	<?php if(!$isEdit): ?> <h2>OCDLA: Post a job</h2> <?php else: ?> <h2>OCDLA: Update a job posting</h2> <?php endif ?>
+	<h2>OCDLA: post a job</h2>
 	<p>Job postings are made available to the public and OCDLA members.  Postings are removed on the close date you specify.  Postings open until filled are removed 6 weeks after the posted date.</p>
 
-	<?php if($isEdit): ?>
-	<strong>Job Id: <?php print $job["Id"]; ?></strong><br /><br />
-	<?php endif ?>
+	<div class="form-item">
+		<label for="Name">Job Title</label><br />
+		<input type="text" name="Name" id="Name" value="<?php print $job["Name"]; ?>" placeholder="Enter your job title." />
+		<input type="hidden" name="Id" id="Id" value="<?php print $job["Id"]; ?>" />
+	</div>
 
-	<label for="Name">Job Title</label><br />
-	<input type="text" name="Name" id="Name" value="<?php print $job["Name"]; ?>" placeholder="Enter your job title." />
-	<input type="hidden" name="Id" id="Id" value="<?php print $job["Id"]; ?>" />
-	
-	<label>Is Active:	</label><input type="checkbox" name="IsActive__c" /><br />
 
-	<label for="Salary__c">Salary</label><br />
-	<input type="text" name="Salary__c" id="Salary__c" value="<?php print $job["Salary__c"]; ?>" placeholder="Enter the salary." />
-	<br /><br />
+	<div class="form-item">
+		<label for="Salary__c">Salary</label><br />
+		<input type="text" name="Salary__c" id="Salary__c" value="<?php print $job["Salary__c"]; ?>" placeholder="Enter the salary." />
+	</div>
 
-	<label for="Location__c">Location</label><br />
-	<input type="text" name="Location__c" id="Location__c" value="<?php print $job["Location__c"]; ?>" placeholder="Enter the location." />
-	<br /><br />
 
-	<label for="PostingDate__c">Posting Date</label><br />
-	<input type="date" name="PostingDate__c" id="PostingDate__c" value="<?php print $job["PostingDate__c"]; ?>" placeholder="Enter the date posted." />
-	<br /><br />
+	<div class="form-item">
+		<label for="Location__c">Location</label><br />
+		<input type="text" name="Location__c" id="Location__c" value="<?php print $job["Location__c"]; ?>" placeholder="Enter the location." />
+	</div>
 
-	<div id="closingDate">
-		<!--beginning closingdate field-->
-		<label for="ClosingDate__c">Closing Date</label><br />
-		<input type="date" name="ClosingDate__c" id="ClosingDate__c" value="<?php print $job["ClosingDate__c"]; ?>" placeholder="Enter the closing date." />
-		<br /><br />
+
+	<div class="form-item">
+		<label for="PostingDate__c">Posting Date</label><br />
+		<input type="date" name="PostingDate__c" id="PostingDate__c" value="<?php print $job["PostingDate__c"]; ?>" placeholder="Enter the date posted." />
 	</div>
 
 
 
-	<div onclick="handleCheck()">
+	<div id="closingDate" class="form-item">
+		<!--beginning closingdate field-->
+		<label for="ClosingDate__c">Closing Date</label><br />
+		<input type="date" name="ClosingDate__c" id="ClosingDate__c" value="<?php print $job["ClosingDate__c"]; ?>" placeholder="Enter the closing date." />
+	</div>
+
+
+	<div class="form-item" onclick="handleCheck()">
 		<label for="OpenUntilFilled__c">Open Until Filled?</label>&nbsp&nbsp
 		<?php if ($job["OpenUntilFilled__c"] == true) : ?>
 			<input type="checkbox" name="OpenUntilFilledHelper__c" id="OpenUntilFilledHelper__c" value="true" checked />
 		<?php else : ?>
 			<input type="checkbox" name="OpenUntilFilledHelper__c" id="OpenUntilFilledHelper__c" value="true" />
 		<?php endif; ?>
-		<br /><br />
 	</div>
-	<?php if ($job["OpenUntilFilled__c"] == true) : ?>
-		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="true" />
-	<?php else : ?>
-		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="false" />
-	<?php endif; ?>
-
-
-	<!-- this is not working yet -->
-	<?php if($isEdit): ?>
-	<strong>Attachments already uploaded for this job:</strong><br />
-	<div class="table" id="job-postings">
-		<tbody>
-			<ul class="table-row">
-				<li class='table-header'>Actions</li>
-				<li class="table-header">Name</li>
-				<li class="table-header">Id</li>
-			</ul>
-			<?php foreach($attachments as $attachment):?>
-			<ul class="table-row">
-				<li class="table-cell cart-middle"><a href="/job/delete/Attachment/<?php print $attachment["Id"] ?>">Delete</a></li>
-				<li class="table-cell cart-middle"><?php print $attachment["Name"] ?></li>
-				<li class="table-cell cart-middle"><?php print $attachment["Id"] ?></li>
-			</ul>
-			 <br />
-			<?php endforeach ?>
-		</tbody>
+	
+	<div class="form-item">
+		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="" />
 	</div>
-	<?php endif ?><br /><br />
 
 
-	<!--changed attachment to an array type-->
-	<label for="Attachments__c[]">Upload Files</label>
-	<input type="file" id="Attachments__c[]" name="Attachments__c[]" >
-	<br /><br />
+	<div class="form-item">
+		<!--changed attachment to an array type-->
+		<label for="Attachments__c[]">Upload Files</label>
+		<input type="file" id="Attachments__c[]" name="Attachments__c[]" >
+	</div>
 
-	<input type="submit" value="Save" />
+
+	<div class="form-item">
+		<input type="submit" value="Save" />
+	</div>
+	
+	
 </form>
 
 <script>
