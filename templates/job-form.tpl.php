5169cc35f00d7c26d9dbc54550495282d9143852
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-07-09 14:07:16 -0700

    layout updates

--- templates/job-form.tpl.php
+++ templates/job-form.tpl.php
@@ -74,6 +74,9 @@ if($hasAttachment || $hasContentDocument){
 	}
 </style>
 
+
+
+
 <form enctype="multipart/form-data" onsubmit="onSubmit();" class="<?php print $classNames; ?>" id="jobs-form" name="form-jobs" method="post" action="/jobs/create">
 
 	<h2>OCDLA: post a job</h2>
@@ -111,27 +114,10 @@ if($hasAttachment || $hasContentDocument){
 		<input type="date" name="ClosingDate__c" id="ClosingDate__c" value="<?php print $job["ClosingDate__c"]; ?>" placeholder="Enter the closing date." />
 	</div>
 
-
-	<div class="form-item" onclick="handleCheck()">
-		<label for="OpenUntilFilled__c">Open Until Filled?</label>&nbsp&nbsp
-		<?php if ($job["OpenUntilFilled__c"] == true) : ?>
-			<input type="checkbox" name="OpenUntilFilledHelper__c" id="OpenUntilFilledHelper__c" value="true" checked />
-		<?php else : ?>
-			<input type="checkbox" name="OpenUntilFilledHelper__c" id="OpenUntilFilledHelper__c" value="true" />
-		<?php endif; ?>
-	</div>
-	<?php if ($job["OpenUntilFilled__c"] == true) : ?>
-		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="true" />
-	<?php else : ?>
-		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="false" />
-	<?php endif; ?>
-	
 	<div class="form-item">
-		<input type="hidden" name="OpenUntilFilled__c" id="OpenUntilFilled__c" value="" />
+		Open until filled?
+		<input type="checkbox" name="OpenUntilFilled__c" onchange="toggleClosingDate()" value="1" checked />
 	</div>
-
-
-
 
 
 
@@ -169,39 +155,18 @@ if($hasAttachment || $hasContentDocument){
 </form>
 
 <script>
-	function onSubmit() {
-		this.openUntilFilled = document.getElementById('OpenUntilFilledHelper__c');
-		this.openUntilFilled.disabled = true;
+	function toggleClosingDate(){
+
+		let elem = document.getElementById("closingDate");
+
+		elem.classList.toggle("hide-element");
+		elem.setAttribute("value", "");
 	}
+
 	function toggleFileUploadElement(e){
 		console.log(e);
 		let theForm = document.getElementById("jobs-form");
 		let hasExisting = theForm.classList.contains("has-attachment");
 		theForm.classList.toggle("has-attachment");
-	}
-
-	//*JavaScript function to toggle closing date in form view*//
-	function handleCheck() {
-
-		//*Variables*//
-		this.jobsForm = document.getElementById('jobs-form');
-		this.openUntilFilledHelper = document.getElementById('OpenUntilFilledHelper__c');
-		this.openUntilFilled = document.getElementById('OpenUntilFilled__c');
-		this.closingDate = document.getElementById("ClosingDate__c");
-		this.isChecked = this.openUntilFilledHelper.checked;
-
-
-		if (isChecked) {
-			this.jobsForm.classList.add('open-until-filled');
-			this.closingDate.disabled = true;
-			//0 out closing date field
-			this.openUntilFilled.value = "true";
-
-
-		} else {
-			this.jobsForm.classList.remove('open-until-filled');
-			this.closingDate.disabled = false;
-			this.openUntilFilled.value = "false";
-		}
 	}
 </script>
