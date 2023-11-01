49cc3534824c2324fa08e36b6846f68e70534cfe
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-11-01 12:27:50 -0700

    Update positng message with help.

--- templates/job-list.tpl.php
+++ templates/job-list.tpl.php
@@ -22,7 +22,7 @@
 <div style="margin-bottom:20px;">
 	<p>Welcome to OCDLA's Job Board. Job postings are removed three days after the Closing Date. Postings that are marked as "Open Until Filled" are removed six weeks after the Posting Date.</p>
 	<p>You may also email your posting description to <a href="mailto:cpainter@ocdla.org">cpainter@ocdla.org</a> and we will post the job. Include the job title, salary, location, and closing date.</p>
-	<p class="warning">We are currently working to fix an issue with Job Posting attachments.  Check back soon to view uploaded supplementary attachments.</p>
+	<p class="warning">We are currently working to fix an issue with Job Posting attachments.  In the meantime, please email your posting attachments to OCDLA.</p>
 </div>
 
 <div class="table" id="job-postings">
